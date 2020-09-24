59e4f691e6a75436d36a61bc564050f8bfddb3fb
engineerIOT <46654184+engineerIOT@users.noreply.github.com>
2020-09-23 22:36:28 -0300

    Add files via upload

---
+++ degrauComDegrau.m
@@ -0,0 +1,17 @@
+clear
+clc
+
+x = inline('4.*((t>=0)&(t<1))+2.*((t>=1)&(t<2))','t')
+
+%intervalo de tempo por oscilação
+t =(-4:0.001:4);
+%t2 =(-2:2);
+
+%valor da função com valores definidos por t
+
+%plotar o gráfico
+plot(t,x(t))
+xlabel('t');
+ylabel('x(t)');
+%grid;
+
