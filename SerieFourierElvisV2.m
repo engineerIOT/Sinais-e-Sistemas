ad96b0abe51d17200a3625d299e2f02713d261c0
engineerIOT <46654184+engineerIOT@users.noreply.github.com>
2020-10-01 11:00:13 -0300

    Add files via upload

---
+++ SerieFourierElvisV2.m
@@ -0,0 +1,26 @@
+clc
+clear
+
+n=50;
+
+intervalo = -3:0.01:3;
+
+indice = 1;
+T= 2;
+w=2.0*pi/T;
+
+for t= intervalo
+    valor = 0.0;
+    for k = -n:n
+        if (k ~=0.0)
+        valor = valor +( ( (2*i*k*w - exp(2*i*k*w)+1) * (exp(i*k*t*w -2*i*w*k)) ) / (2*(k^2)*w^2));
+        else
+            valor = valor +1;
+            
+        end
+    end
+    res (indice) = valor;
+    indice = indice +1;
+end
+
+plot (intervalo, res);
